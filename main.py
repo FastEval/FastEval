35397f88b6960c03d3df2f27845a021750b66ca2
Til Jasper Ullrich <git@tju.me>
2023-05-15 20:52:39 +0000

    Evaluate gpt-3.5-turbo on new evals like MMLU

--- main.py
+++ main.py
@@ -171,5 +171,5 @@ def evaluate_model(model_name):
     build_reports_index(model_name)
 
 if __name__ == '__main__':
-    evaluate_model('oasst-rlhf-2-llama-30b-7k-steps')
-    # evaluate_model('gpt-3.5-turbo')
+    # evaluate_model('oasst-rlhf-2-llama-30b-7k-steps')
+    evaluate_model('gpt-3.5-turbo')
