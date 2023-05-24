d0cd2f2e3ad9c6088d14539d0d327954db72e0d4
Til Jasper Ullrich <git@tju.me>
2023-05-24 16:32:19 +0200

    WIP

--- evaluation/benchmarks/lm_evaluation_harness.py
+++ evaluation/benchmarks/lm_evaluation_harness.py
@@ -1,8 +1,11 @@
+import lm_eval.evaluator
+
 from evaluation.utils import get_model_type
 
 def evaluate_model(model):
     model_type = get_model_type(model)
     print('lm-evaluation-harness: Evaluating', model, model_type)
+    lm_eval.evaluator.simple_evaluate(model, write_out=True, output_base_path='./reports/lm-evaluation-harness')
 
 def evaluate_models(models):
     for model_name in models:
