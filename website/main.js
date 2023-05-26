838917160f3de3a62104f606a6b98096948f2f89
Til Jasper Ullrich <git@tju.me>
2023-05-26 22:18:27 +0200

    Website: Fix reports fetching on local webserver

--- website/main.js
+++ website/main.js
@@ -5,7 +5,11 @@ async function main() {
         location.reload()
     })
 
-    const benchmarksV = await createBenchmarksV('https://raw.githubusercontent.com/tju01/oasst-automatic-model-eval/main/reports')
+    const url = location.hostname === 'tju01.github.io'
+        ? 'https://raw.githubusercontent.com/tju01/oasst-automatic-model-eval/main/reports'
+        : './reports'
+
+    const benchmarksV = await createBenchmarksV(url)
     document.body.appendChild(benchmarksV)
 }
 
