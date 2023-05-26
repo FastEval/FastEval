4e03ca5ee597c0ab8031f9c20f28cb0a2788c4e8
Til Jasper Ullrich <git@tju.me>
2023-05-26 15:24:59 +0200

    Fix system -> prompter message conversion

--- evaluation/models/utils.py
+++ evaluation/models/utils.py
@@ -3,7 +3,7 @@ def put_system_message_in_prompter_message(conversation):
     current_prompter_message = None
     for item_type, item in conversation:
         if item_type == 'system':
-            current_prompter_message += item
+            current_prompter_message = item
         elif item_type == 'assistant':
             if current_prompter_message is not None:
                 raise
@@ -13,6 +13,7 @@ def put_system_message_in_prompter_message(conversation):
                 new_conversation.append((item_type, item))
             else:
                 new_conversation.append((item_type, current_prompter_message + '\n\n' + item))
+                current_prompter_message = None
         else:
             raise
     return new_conversation
