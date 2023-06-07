25056817c67d389d3cb168741e11089231755ea9
Til Jasper Ullrich <git@tju.me>
2023-06-07 23:12:31 +0200

    Use get_dtype for lm-evaluation-harness

--- evaluation/benchmarks/lm_evaluation_harness.py
+++ evaluation/benchmarks/lm_evaluation_harness.py
@@ -3,25 +3,28 @@ import json
 
 import lm_eval.evaluator
 
-from ..utils import replace_model_name_slashes
+from ..utils import replace_model_name_slashes, get_dtype
 
 def evaluate_model(model_type, model_name):
+    if model_type == 'openai':
+        return
+
     output_path = os.path.join('./reports/lm-evaluation-harness', replace_model_name_slashes(model_name) + '.json')
     if os.path.exists(output_path):
         return
 
     tasks = ['openbookqa', 'arc_easy', 'winogrande', 'hellaswag', 'arc_challenge', 'piqa', 'boolq']
 
-    if model_type == 'openai':
-        return
+    model_args = ','.join([k + '=' + v for k, v in {
+        'pretrained': model_name,
+        'dtype': str(get_dtype(model_type, model_name)).replace('torch.', ''),
+        'trust_remote_code': True,
+        'use_accelerate': True,
+    }])
 
     print('lm-evaluation-harness: Evaluating', model_name)
-    if model_type == 'falcon-instruct':
-        dtype = 'bfloat16'
-    else:
-        dtype = 'float16'
-    results = lm_eval.evaluator.simple_evaluate('hf-causal-experimental', tasks=tasks,
-        model_args='pretrained=' + model_name + ',dtype="' + dtype + '",trust_remote_code=True,use_accelerate=True')
+
+    results = lm_eval.evaluator.simple_evaluate('hf-causal-experimental', tasks=tasks, model_args=model_args)
 
     with open(output_path, 'w') as f:
         json.dump(results, f, indent=4)
