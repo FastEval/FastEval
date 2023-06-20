76452993e7f69681b68de874f32e74b0132a2f67
Til Jasper Ullrich <git@tju.me>
2023-06-21 01:45:23 +0200

    Initial total ranking for models using Elo

--- website/benchmarks/main.js
+++ website/benchmarks/main.js
@@ -22,6 +22,64 @@ async function createSingleBenchmarkV(baseUrl, benchmarkName, parameters) {
     }
 }
 
+function computeModelRanksSingleSeed(modelNames, allBenchmarks, modelsByName, getScore) {
+    const SCALE = 400
+    const BASE = 10
+    const K = 32
+
+    const modelRanks = Object.fromEntries(modelNames.map(modelName => [modelName, 1000]))
+    for (let i = 0; i < 100; i++) {
+        const model1Name = modelNames[Math.floor(Math.random() * modelNames.length)]
+        const model2Name = modelNames[Math.floor(Math.random() * modelNames.length)]
+
+        let model1Score
+        let model2Score
+        for (let j = 0; j < 1; j++) {
+            const benchmarkName = allBenchmarks[Math.floor(Math.random() * allBenchmarks.length)]
+
+            const model1 = modelsByName[model1Name]
+            const model2 = modelsByName[model2Name]
+
+            model1Score = getScore(model1Name, model1.benchmarks, benchmarkName)
+            model2Score = getScore(model2Name, model2.benchmarks, benchmarkName)
+
+            if (model1Score !== null && model2Score !== null)
+                break
+        }
+
+        if (model1Score === null || model2Score === null)
+            continue
+
+        const e1 = 1 / (1 + BASE ** ((modelRanks[model2Name] - modelRanks[model1Name]) / SCALE))
+        const e2 = 1 / (1 + BASE ** ((modelRanks[model1Name] - modelRanks[model2Name]) / SCALE))
+
+        const sa = model1Score > model2Score ? 1
+            : model2Score > model1Score ? 0
+            : 0.5
+
+        modelRanks[model1Name] += K * (sa - e1)
+        modelRanks[model2Name] += K * (1 - sa - e2)
+    }
+
+    return modelRanks
+}
+
+function computeModelRanks(models, getScore, allBenchmarks) {
+    const modelNames = [...new Set(models.map(({ model_name: model }) => model))]
+    const modelsByName = Object.fromEntries(models.map(model => [model.model_name, model]))
+
+    const numSeeds = 1000
+    const modelRanks = Object.fromEntries(modelNames.map(modelName => [modelName, 0]))
+    for (let i = 0; i < numSeeds; i++) {
+        const ranks = computeModelRanksSingleSeed(modelNames, allBenchmarks, modelsByName, getScore)
+        for (const [modelName, rank] of Object.entries(ranks)) {
+            modelRanks[modelName] += rank / numSeeds
+        }
+    }
+
+    return modelRanks
+}
+
 export async function createBenchmarksIndexV(baseUrl) {
     const models = (await (await fetch(baseUrl + '/__index__.json')).json())
 
@@ -58,9 +116,11 @@ export async function createBenchmarksIndexV(baseUrl) {
         return null
     }
 
+    const allBenchmarks = ['lm-evaluation-harness', 'vicuna', 'openai-evals', 'human-eval-plus']
+
     const benchmarkMinimums = new Map()
     const benchmarkMaximums = new Map()
-    for (const benchmarkName of ['lm-evaluation-harness', 'vicuna', 'openai-evals', 'human-eval-plus']) {
+    for (const benchmarkName of allBenchmarks) {
         for (const { model_name: model, benchmarks } of models) {
             const score = getScore(model, benchmarks, benchmarkName)
             if (score === null)
@@ -77,8 +137,28 @@ export async function createBenchmarksIndexV(baseUrl) {
         }
     }
 
+    function getRelativeScore(model, benchmarks, benchmarkName) {
+        const score = getScore(model, benchmarks, benchmarkName)
+        return (score - benchmarkMinimums.get(benchmarkName))
+            / (benchmarkMaximums.get(benchmarkName) - benchmarkMinimums.get(benchmarkName))
+    }
+
+    const modelRanks = computeModelRanks(models, getRelativeScore, allBenchmarks)
+    const modelsSortedByRank = models.toSorted((model1, model2) => {
+        const model1Rank = modelRanks[model1.model_name]
+        const model2Rank = modelRanks[model2.model_name]
+        return model2Rank - model1Rank
+    })
+
+    const modelRanksMin = Math.min(...Object.entries(modelRanks).map(([modelName, rank]) => rank))
+    const modelRanksMax = Math.max(...Object.entries(modelRanks).map(([modelName, rank]) => rank))
+    const normalizedModelRanks = Object.fromEntries(Object.entries(modelRanks).map(([modelName, modelRank]) =>
+        [modelName, (modelRank - modelRanksMin) / (modelRanksMax - modelRanksMin)]))
+
     const tableE = document.createElement('table')
+
     const theadE = tableE.createTHead().insertRow()
+    theadE.insertCell().appendChild(createTextE('Total Rank'))
     theadE.insertCell().appendChild(createTextE('Model'))
     theadE.insertCell().appendChild(createTextE('Total'))
     theadE.insertCell().appendChild(createLinkE('lm-evaluation-harness', { benchmark: 'lm-evaluation-harness' }))
@@ -86,8 +166,12 @@ export async function createBenchmarksIndexV(baseUrl) {
     theadE.insertCell().appendChild(createLinkE('OpenAI Evals', { benchmark: 'openai-evals' }))
     theadE.insertCell().appendChild(createTextE('HumanEval+'))
     const tbodyE = tableE.createTBody()
-    for (const { model_name: model, benchmarks } of models) {
+
+    for (const { model_name: model, benchmarks } of modelsSortedByRank) {
         const rowE = tbodyE.insertRow()
+
+        createTableScoreCell(rowE, createTextE(round(normalizedModelRanks[model])))
+
         rowE.insertCell().appendChild(createTextE(model))
 
         const allBenchmarkEvaluated = benchmarks.includes('lm-evaluation-harness')
@@ -95,39 +179,32 @@ export async function createBenchmarksIndexV(baseUrl) {
             && benchmarks.includes('openai-evals')
             && benchmarks.includes('human-eval-plus')
         if (allBenchmarkEvaluated) {
-            const allBenchmarks = ['lm-evaluation-harness', 'vicuna', 'openai-evals', 'human-eval-plus']
             let relativeAverageScore = 0
-            for (const benchmarkName of allBenchmarks) {
-                const score = getScore(model, benchmarks, benchmarkName)
-                const relativeScore = (score - benchmarkMinimums.get(benchmarkName))
-                    / (benchmarkMaximums.get(benchmarkName) - benchmarkMinimums.get(benchmarkName))
-                relativeAverageScore += relativeScore / allBenchmarks.length
-            }
+            for (const benchmarkName of allBenchmarks)
+                relativeAverageScore += getRelativeScore(model, benchmarks, benchmarkName) / allBenchmarks.length
 
             createTableScoreCell(rowE, createTextE(round(relativeAverageScore)))
         } else {
             createTableScoreCell(rowE, createTextE(''))
         }
 
-        if (benchmarks.includes('lm-evaluation-harness'))
-            createTableScoreCell(rowE, createTextE(round(averageLmEvaluationHarnessScores[model])))
-        else
-            createTableScoreCell(rowE, createTextE(''))
+        for (const benchmarkName of allBenchmarks) {
+            const score = getScore(model, benchmarks, benchmarkName)
+            if (score === null) {
+                createTableScoreCell(rowE, createTextE(''))
+                continue
+            }
 
-        if (benchmarks.includes('vicuna') && model in vicunaEvaluationResults.models)
-            createTableScoreCell(rowE, createTextE(Math.round(vicunaEvaluationResults.models[model].elo_rank)))
-        else
-            rowE.insertCell()
+            let text = round(score)
+            if (benchmarkName === 'vicuna')
+                text = Math.round(score)
+            if (benchmarkName === 'human-eval-plus') {
+                createTableScoreCell(rowE, createLinkE(text, { benchmark: 'human-eval-plus', model }))
+                continue
+            }
 
-        if (benchmarks.includes('openai-evals'))
-            createTableScoreCell(rowE, createTextE(round(relativeOpenAiEvalsScores[model])))
-        else
-            rowE.insertCell()
-
-        if (benchmarks.includes('human-eval-plus'))
-            createTableScoreCell(rowE, createLinkE(round(humanEvalPlusResultsMap[model].score), { benchmark: 'human-eval-plus', model }))
-        else
-            createTableScoreCell(rowE, createTextE(''))
+            createTableScoreCell(rowE, createTextE(text))
+        }
     }
 
     return tableE
