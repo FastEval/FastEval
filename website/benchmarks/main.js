c515b103978dc11b15fc40e3f48927915d59a219
Til Jasper Ullrich <git@tju.me>
2023-06-21 17:31:24 +0200

    Another method

--- website/benchmarks/main.js
+++ website/benchmarks/main.js
@@ -26,61 +26,72 @@ function computeModelRanks(models, getScore, allBenchmarks) {
     const modelNames = [...new Set(models.map(({ model_name: model }) => model))]
     const modelsByName = Object.fromEntries(models.map(model => [model.model_name, model]))
 
-    function loss(rankings) {
-        let totalLoss = 0
-        for (const model1Name of modelNames) {
-            for (const model2Name of modelNames) {
-                if (model1Name === model2Name)
-                    continue
-
-                const commonBenchmarks = modelsByName[model1Name].benchmarks
-                    .filter(benchmark => modelsByName[model2Name].benchmarks.includes(benchmark))
-
-                let performanceDifference = 0
-                for (const benchmarkName of commonBenchmarks) {
-                    const model1Performance = getScore(model1Name, commonBenchmarks, benchmarkName)
-                    const model2Performance = getScore(model2Name, commonBenchmarks, benchmarkName)
-                    performanceDifference += (model1Performance - model2Performance) / commonBenchmarks.length
-                }
-
-                const rank1 = rankings[model1Name]
-                const rank2 = rankings[model2Name]
-                const rankDifference = rank1 - rank2
-
-                const disagreement = (rankDifference > 0 && performanceDifference < 0 || rankDifference < 0 && performanceDifference > 0) ? 1 : 0
-                totalLoss += disagreement
-            }
-        }
-
-        return totalLoss
-    }
-
-    let bestRankings
-    let bestLoss = Infinity
-    for (let j = 0; j < 1000; j++) {
-        const numIterations = 1000
-        let currentLoss = Infinity
-        let currentRankings = Object.fromEntries(modelNames.map(modelName => [modelName, Math.random()]))
-        for (let i = 1; i < numIterations; i++) {
-            const newRankings = { ...currentRankings }
-            for (const modelName in newRankings)
-                newRankings[modelName] += (Math.random() - 0.5) * (1 - i / numIterations)
-            const newLoss = loss(newRankings)
-            if (newLoss <= currentLoss) {
-                currentLoss = newLoss
-                currentRankings = newRankings
-            }
-        }
-
-        if (currentLoss < bestLoss) {
-            bestLoss = currentLoss
-            bestRankings = currentRankings
+    const modelPairs = []
+    for (const model1Name of modelNames) {
+        for (const model2Name of modelNames) {
+            if (model1Name === model2Name)
+                continue
+            modelPairs.push([model1Name, model2Name])
         }
     }
 
+    const performanceDifferences = new Map()
+    for (const modelPair of modelPairs) {
+        const [model1Name, model2Name] = modelPair
 
+        const commonBenchmarks = modelsByName[model1Name].benchmarks
+            .filter(benchmark => modelsByName[model2Name].benchmarks.includes(benchmark))
 
-    return bestRankings
+        let performanceDifference = 0
+        for (const benchmarkName of commonBenchmarks) {
+            const model1Performance = getScore(model1Name, commonBenchmarks, benchmarkName)
+            const model2Performance = getScore(model2Name, commonBenchmarks, benchmarkName)
+            performanceDifference += (model1Performance - model2Performance) / commonBenchmarks.length
+        }
+
+        performanceDifferences.set(modelPair, performanceDifference)
+    }
+
+    function lossf(rankings) {
+        return modelPairs.map(modelPair => {
+            const [model1Name, model2Name] = modelPair
+            const performanceDifference = performanceDifferences.get(modelPair)
+            const rankDifference = rankings.get(model1Name) - rankings.get(model2Name)
+            return (rankDifference > 0 && performanceDifference < 0 || rankDifference < 0 && performanceDifference > 0) ? 1 : 0
+        }).reduce((a, b) => a + b, 0)
+    }
+
+    const populationSize = 100
+    let population = []
+    for (let i = 0; i < populationSize; i++) {
+        const rankings = new Map(modelNames.map(modelName => [modelName, Math.random()]))
+        const loss = lossf(rankings)
+        population.push([rankings, loss])
+    }
+
+    const numIterations = 20_000
+    for (let i = 0; i < numIterations; i++) {
+        const currentItemIndex = Math.floor(Math.random() * population.length)
+        const [currentRanking, currentLoss] = population[currentItemIndex]
+
+        const newRanking = new Map(currentRanking)
+        for (const modelName of newRanking.keys())
+            newRanking.set(modelName, newRanking.get(modelName) + (Math.random() > 0.5 ? 1 : -1) * (1 - i / numIterations))
+        const newLoss = lossf(newRanking)
+
+        if (newLoss > currentLoss)
+            continue
+        else if (newLoss === currentLoss)
+            population[currentItemIndex] = [newRanking, newLoss]
+        else
+            population.push([newRanking, newLoss])
+
+        if (i % Math.round(populationSize / 5) === 0)
+            population = population.toSorted(([ranking1, loss1], [ranking2, loss2]) => loss1 - loss2)
+                .slice(0, Math.ceil((1 - i / numIterations) * populationSize))
+    }
+
+    return Object.fromEntries(population.toSorted(([ranking1, loss1], [ranking2, loss2]) => loss1 - loss2)[0][0].entries())
 }
 
 export async function createBenchmarksIndexV(baseUrl) {
@@ -153,15 +164,10 @@ export async function createBenchmarksIndexV(baseUrl) {
         return model2Rank - model1Rank
     })
 
-    const modelRanksMin = Math.min(...Object.entries(modelRanks).map(([modelName, rank]) => rank))
-    const modelRanksMax = Math.max(...Object.entries(modelRanks).map(([modelName, rank]) => rank))
-    const normalizedModelRanks = Object.fromEntries(Object.entries(modelRanks).map(([modelName, modelRank]) =>
-        [modelName, (modelRank - modelRanksMin) / (modelRanksMax - modelRanksMin)]))
-
     const tableE = document.createElement('table')
 
     const theadE = tableE.createTHead().insertRow()
-    theadE.insertCell().appendChild(createTextE('Total Rank'))
+    theadE.insertCell().appendChild(createTextE('Rank'))
     theadE.insertCell().appendChild(createTextE('Model'))
     theadE.insertCell().appendChild(createTextE('Total'))
     theadE.insertCell().appendChild(createLinkE('lm-evaluation-harness', { benchmark: 'lm-evaluation-harness' }))
@@ -170,10 +176,10 @@ export async function createBenchmarksIndexV(baseUrl) {
     theadE.insertCell().appendChild(createTextE('HumanEval+'))
     const tbodyE = tableE.createTBody()
 
-    for (const { model_name: model, benchmarks } of modelsSortedByRank) {
+    for (const [position, { model_name: model, benchmarks }] of modelsSortedByRank.entries()) {
         const rowE = tbodyE.insertRow()
 
-        createTableScoreCell(rowE, createTextE(round(normalizedModelRanks[model])))
+        createTableScoreCell(rowE, createTextE(position + 1))
 
         rowE.insertCell().appendChild(createTextE(model))
 
