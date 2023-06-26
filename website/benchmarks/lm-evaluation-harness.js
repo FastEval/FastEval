c46d512ec313cb4b7b3c4532998f515fe48a6f52
Til Jasper Ullrich <git@tju.me>
2023-06-26 18:56:49 +0200

    Improve Website

--- website/benchmarks/lm-evaluation-harness.js
+++ website/benchmarks/lm-evaluation-harness.js
@@ -2,6 +2,7 @@ import { createTextE } from '../components/text.js'
 import { createBackToMainPageE } from '../components/back-to-main-page.js'
 import { createModelsMap, round } from '../utils.js'
 import { createModelLinkE } from '../components/model-link.js'
+import { createTableScoreCell } from '../components/table-score-cell.js'
 
 export function computeAverageScore(results) {
     return Object.values(results).map(r => r.acc_norm ?? r.acc).reduce((a, b) => a + b, 0) / Object.values(results).length * 100
@@ -16,12 +17,18 @@ export async function createV(baseUrl) {
     otherResultsE.classList.add('lm-evaluation-harness__other-results')
     containerE.appendChild(otherResultsE)
 
+    const lmEvaluationHarnessLinkE = document.createElement('a')
+    lmEvaluationHarnessLinkE.textContent = 'lm-evaluation-harness'
+    lmEvaluationHarnessLinkE.href = 'https://github.com/EleutherAI/lm-evaluation-harness'
+
     const otherResultsLinkE = document.createElement('a')
     otherResultsLinkE.textContent = 'here'
     otherResultsLinkE.href = 'https://gpt4all.io/index.html'
 
     otherResultsE.append(
-        createTextE('This table is computed using the same method as the one used for the gpt4all leaderboard and therefore the numbers are comparable. '
+        createTextE('This benchmark computes the average over some tasks from '),
+        lmEvaluationHarnessLinkE,
+        createTextE('. The method is the same as for the gpt4all leaderboard and therefore the numbers are comparable. '
             + 'You can view the gpt4all leaderboard '),
         otherResultsLinkE,
         createTextE('. Scroll down to the section "Performance Benchmarks".'),
@@ -34,8 +41,17 @@ export async function createV(baseUrl) {
 
     const results = await Promise.all(modelNames.map(async model =>
         [model, await fetch(baseUrl + '/lm-evaluation-harness/' + model.replace('/', '--') + '.json').then(r => r.json())]))
-    const tasks = ['boolq', 'piqa', 'hellaswag', 'winogrande', 'arc_easy', 'arc_challenge', 'openbookqa']
     const resultsMap = Object.fromEntries(results)
+
+    const tasks = [
+        ['boolq', 'BoolQ'],
+        ['piqa', 'PIQA'],
+        ['hellaswag', 'HellaSwag'],
+        ['winogrande', 'WinoGrande'],
+        ['arc_easy', 'ARC-e'],
+        ['arc_challenge', 'ARC-c'],
+        ['openbookqa', 'OBQA'],
+    ]
 
     const reportsIndexE = document.createElement('table')
     containerE.appendChild(reportsIndexE)
@@ -43,9 +59,9 @@ export async function createV(baseUrl) {
     const tableBodyE = reportsIndexE.createTBody()
     tableHeadE.insertCell().appendChild(createTextE('Model'))
 
-    for (const task of tasks)
-        tableHeadE.insertCell().appendChild(createTextE(task))
     tableHeadE.insertCell().appendChild(createTextE('Average'))
+    for (const [taskId, taskName] of tasks)
+        tableHeadE.insertCell().appendChild(createTextE(taskName))
 
     const averageScores = Object.fromEntries(modelNames.map(modelName => [modelName, computeAverageScore(resultsMap[modelName].results)]))
     const modelNamesSortedByAverageScore = modelNames.sort((model1Name, model2Name) => averageScores[model2Name] - averageScores[model1Name])
@@ -54,13 +70,13 @@ export async function createV(baseUrl) {
         const rowE = tableBodyE.insertRow()
         rowE.insertCell().appendChild(createModelLinkE(modelsMap[modelName]))
 
-        for (const task of tasks) {
-            let r = resultsMap[modelName].results[task]
-            r = (r.acc_norm ?? r.acc) * 100
-            rowE.insertCell().appendChild(createTextE(round(r)))
-        }
+        createTableScoreCell(rowE, createTextE(round(averageScores[modelName])))
 
-        rowE.insertCell().appendChild(createTextE(round(averageScores[modelName])))
+        for (const [taskId, taskName] of tasks) {
+            let r = resultsMap[modelName].results[taskId]
+            r = (r.acc_norm ?? r.acc) * 100
+            createTableScoreCell(rowE, createTextE(round(r)))
+        }
     }
 
     return containerE
