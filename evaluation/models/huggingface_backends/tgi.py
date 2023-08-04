5ba2ae685b7f379092cc5efc2020229e584ceae4
Til Jasper Ullrich <git@tju.me>
2023-08-04 12:27:47 +0000

    Extend (simple) data parallel evaluation to TGI backend

--- evaluation/models/huggingface_backends/tgi.py
+++ evaluation/models/huggingface_backends/tgi.py
@@ -5,27 +5,9 @@ import torch
 import threading
 import subprocess
 
-import transformers
 from text_generation import Client
 
-import evaluation.args
-import evaluation.models.models
-
-lock = threading.Lock()
-server_information = None
-
-def unload_model(use_lock=True):
-    global server_information
-
-    if use_lock:
-        lock.acquire()
-
-    if server_information is not None:
-        server_information['server']['process'].terminate()
-        server_information = None
-
-    if use_lock:
-        lock.release()
+from evaluation.models.huggingface_backends.data_parallel import DataParallelBackend
 
 def should_filter_process_output(line):
     if 'GenerateParameters' in line and 'text_generation_router' in line and 'Success' in line:
@@ -39,8 +21,6 @@ def print_process_output(stdout):
             print('[TGI]', line, end='')
 
 def start_server(*, model_path, tokenizer_path, dtype):
-    global server_is_ready
-
     cwd = os.getcwd()
 
     new_environment = os.environ.copy()
@@ -58,11 +38,6 @@ def start_server(*, model_path, tokenizer_path, dtype):
     else:
         raise Exception('This dtype is not supported by text-generation-inference')
 
-    if evaluation.args.cmd_arguments.tgi_max_batch_total_tokens is not None:
-        additional_args = ['--max-batch-total-tokens', str(evaluation.args.cmd_arguments.tgi_max_batch_total_tokens)]
-    else:
-        additional_args = []
-
     process = subprocess.Popen([
         'text-generation-launcher',
         '--model-id', model_path,
@@ -73,7 +48,6 @@ def start_server(*, model_path, tokenizer_path, dtype):
         '--dtype', dtype_arg,
         '--max-concurrent-requests', '1024',
         '--num-shard', str(torch.cuda.device_count()),
-        *additional_args,
     ], env=new_environment, stdout=subprocess.PIPE, text=True)
 
     for line in process.stdout:
@@ -91,31 +65,10 @@ def start_server(*, model_path, tokenizer_path, dtype):
         'port': port,
     }
 
-def run_inference(*, prompt, tokenizer_path, model_path, dtype, max_new_tokens, temperature):
-    global server_information
+def compute_model_response(*, model, item):
+    client = Client('http://127.0.0.1:' + str(model['port']), timeout=1_000_000)
 
-    lock.acquire()
-
-    evaluation.models.models.switch_gpu_model_type('tgi')
-
-    if (server_information is None
-            or server_information['tokenizer_path'] != tokenizer_path
-            or server_information['model_path'] != model_path
-            or server_information['dtype'] != dtype):
-        unload_model(False)
-        server_information = {
-            'tokenizer_path': tokenizer_path,
-            'model_path': model_path,
-            'dtype': dtype,
-            'tokenizer': transformers.AutoTokenizer.from_pretrained(tokenizer_path),
-            'server': start_server(model_path=model_path, tokenizer_path=tokenizer_path, dtype=dtype),
-        }
-
-    current_server_information = server_information
-    client = Client('http://127.0.0.1:' + str(current_server_information['server']['port']), timeout=1_000_000)
-
-    lock.release()
-
+    temperature = item['temperature']
     if temperature is None:
         temperature = 1.0
     if temperature > 1e-8:
@@ -123,10 +76,32 @@ def run_inference(*, prompt, tokenizer_path, model_path, dtype, max_new_tokens, 
     else:
         kwargs = { 'do_sample': False }
 
-    return client.generate(prompt,
+    max_new_tokens = item['max_new_tokens']
+    assert max_new_tokens is not None
+
+    return client.generate(item['prompt'],
         max_new_tokens=max_new_tokens,
         repetition_penalty=1.0,
         return_full_text=False,
         best_of=1,
         **kwargs,
     ).generated_text
+
+def unload_worker_model(model):
+    model['process'].terminate()
+
+backend = DataParallelBackend(
+    backend_name='tgi',
+    worker_functions={
+        'create_model': start_server,
+        'compute_model_response': compute_model_response,
+        'unload_worker_model': unload_worker_model,
+    },
+    worker_is_blocking=False,
+)
+
+def run_inference(**kwargs):
+    return backend.run_inference(**kwargs, max_batch_size=1)
+
+def unload_model():
+    return backend.unload_model()
