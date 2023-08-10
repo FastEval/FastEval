0f3a985407329eeeb82d044337f9a4f6517ee0c7
Til Jasper Ullrich <git@tju.me>
2023-08-10 15:55:49 +0000

    Add data parallel evaluation for LM-Eval

--- evaluation/benchmarks/lm_evaluation_harness.py
+++ evaluation/benchmarks/lm_evaluation_harness.py
@@ -1,30 +1,48 @@
 import os
 import json
 import statistics
+import subprocess
+import shutil
 
+import evaluation.args
 from evaluation.benchmarks.utils import model_name_to_filename
 from evaluation.models.models import get_dtype
 
-def run_evaluation(*, model_name, model_args):
-    import lm_eval.evaluator
-
+def run_evaluation(*, model_name, model_args, output_path):
     tasks = ['openbookqa', 'arc_easy', 'winogrande', 'hellaswag', 'arc_challenge', 'piqa', 'boolq']
 
     kwargs = {}
     if 'tokenizer' in model_args:
         kwargs['tokenizer'] = model_args['tokenizer']
 
-    lm_eval_model_args = ','.join([k + '=' + str(v) for k, v in ({
-        'pretrained': model_name,
-        'dtype': str(get_dtype(model_name)).replace('torch.', ''),
-        'trust_remote_code': True,
-        'use_accelerate': True,
-        **kwargs,
-    }).items()])
+    def build_lm_eval_command(*, parallelize):
+        lm_eval_model_args = ','.join([k + '=' + str(v) for k, v in ({
+            'pretrained': model_name,
+            'dtype': str(get_dtype(model_name)).replace('torch.', ''),
+            'trust_remote_code': True,
+            'parallelize': parallelize,
+            **kwargs,
+        }).items()])
+
+        cmd = [
+            shutil.which('lm_eval'),
+            '--batch_size', 'auto:5',
+            '--tasks', ','.join(tasks),
+            '--model', 'hf',
+            '--model_args', lm_eval_model_args,
+            '--output_path', output_path,
+        ]
+
+        return cmd
+
+    if evaluation.args.cmd_arguments.num_gpus_per_model == 1:
+        cmd = ['accelerate', 'launch'] + build_lm_eval_command(parallelize=False)
+    else:
+        cmd = build_lm_eval_command(parallelize=True)
 
     print(model_name + ' :: LM-Eval :: Evaluating')
 
-    return lm_eval.evaluator.simple_evaluate('hf-causal-experimental', tasks=tasks, model_args=lm_eval_model_args)
+    subprocess.run(cmd)
 
 def evaluate_model(model_type, model_name, model_args, evaluation_id):
     if model_type == 'openai':
@@ -34,13 +52,11 @@ def evaluate_model(model_type, model_name, model_args, evaluation_id):
     os.makedirs(output_folder, exist_ok=True)
 
     gpt4all_output_filepath = os.path.join(output_folder, 'gpt4all.json')
-    if os.path.exists(gpt4all_output_filepath):
-        with open(gpt4all_output_filepath) as f:
-            results = json.load(f)
-    else:
-        results = run_evaluation(model_name=model_name, model_args=model_args)
-        with open(gpt4all_output_filepath, 'w') as f:
-            json.dump(results, f, indent=4)
+    if not os.path.exists(gpt4all_output_filepath):
+        run_evaluation(model_name=model_name, model_args=model_args, output_path=gpt4all_output_filepath)
+
+    with open(gpt4all_output_filepath) as f:
+        results = json.load(f)
 
     total_scores_filepath = os.path.join(output_folder, 'total.json')
     if os.path.exists(total_scores_filepath):
@@ -49,7 +65,8 @@ def evaluate_model(model_type, model_name, model_args, evaluation_id):
     scores = { 'tasks': {} }
     for task_name in results['results'].keys():
         task_results = results['results'][task_name]
-        scores['tasks'][task_name] = 100 * (task_results['acc_norm'] if 'acc_norm' in task_results else task_results['acc'])
+        score = task_results.get('acc_norm') or task_results.get('acc') or task_results.get('acc_norm,none') or task_results.get('acc,none')
+        scores['tasks'][task_name] = 100 * score
 
     scores['average'] = statistics.mean(scores['tasks'].values())
 
