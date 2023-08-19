6e8ad82c4cea6dcc8016c0423e27ad5486e026e9
Til Jasper Ullrich <git@tju.me>
2023-08-19 22:04:54 +0000

    Make more tasks work

--- evaluation/benchmarks/agentbench.py
+++ evaluation/benchmarks/agentbench.py
@@ -25,11 +25,12 @@ def evaluate_model(model_type, model_name, model_args, evaluation_id):
 
     # Clone repository, setup virtual environment
 
-    subprocess.run(['git', 'clone', 'https://github.com/THUDM/AgentBench.git'], cwd='.tmp')
+    subprocess.run(['git', 'clone', 'https://github.com/FastEval/AgentBench.git'], cwd='.tmp')
     subprocess.run(['python3', '-m', 'venv', 'venv'], cwd='.tmp/AgentBench')
 
     new_environment = os.environ.copy()
     new_environment['PATH'] = os.path.join(os.getcwd(), '.tmp/AgentBench/venv/bin') + ':' + os.environ['PATH']
+    new_environment['OPENAI_API_BASE'] = 'https://api.openai.com/v1' # For lateral thinking puzzle
 
     subprocess.run(['pip', 'install', '--upgrade', 'pip'], env=new_environment)
     subprocess.run(['pip', 'install', '-r', 'requirements.txt'], cwd='.tmp/AgentBench', env=new_environment)
@@ -51,6 +52,11 @@ def evaluate_model(model_type, model_name, model_args, evaluation_id):
 
     subprocess.run(['pip', 'install', '-r', 'src/tasks/dbbench/requirements.txt'], cwd='.tmp/AgentBench', env=new_environment)
 
+    # Task: Lateral Thinking Puzzle
+
+    subprocess.run(['pip', 'install', '-r', 'src/tasks/lateralthinkingpuzzle/requirements.txt'], cwd='.tmp/AgentBench', env=new_environment)
+    subprocess.run(['pip', 'install', 'openpyxl'], env=new_environment) # Missing dependency
+
     # Task: Knowledge Graph
 
     # This setup is way too complicated.
@@ -66,7 +72,19 @@ def evaluate_model(model_type, model_name, model_args, evaluation_id):
 
     agent_file = os.path.join(os.getcwd(), 'evaluation', 'benchmarks', 'agentbench_agent.yaml')
 
-    subprocess.run(['python', 'eval.py', '--task', 'configs/tasks/example.yaml',
-        '--agent', agent_file], env=new_environment, cwd='.tmp/AgentBench')
+    tasks = [
+        # 'configs/tasks/os_interaction/dev.yaml', # Runs
+        #'configs/tasks/dbbench/dev.yaml', # MySQL connection error towards the end
+        # 'configs/tasks/lateralthinkingpuzzle/dev.yaml', # Runs
+        'configs/tasks/lateralthinkingpuzzle_zh/dev.yaml',
+        # 'configs/tasks/knowledgegraph/dev.yaml', # Excluded for now
+        #'configs/tasks/alfworld/dev.yaml',
+        #'configs/tasks/mind2web/dev.yaml',
+        #'configs/tasks/card_game/dev.yaml',
+    ]
+
+    for task in tasks:
+        subprocess.run(['python', 'eval.py', '--task', task, '--agent', agent_file, '--workers', str(os.cpu_count())],
+            env=new_environment, cwd='.tmp/AgentBench')
 
     server.shutdown()
