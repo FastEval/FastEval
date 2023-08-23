008d1eecb864688cdcbab87df3d72aa43127df3c
Til Jasper Ullrich <git@tju.me>
2023-08-23 15:34:20 +0000

    stuff

--- evaluation/benchmarks/agentbench.py
+++ evaluation/benchmarks/agentbench.py
@@ -17,25 +17,38 @@ class InferenceRequestHandler(http.server.BaseHTTPRequestHandler):
     def do_POST(self):
         request = json.loads(self.rfile.read(int(self.headers.get('Content-Length'))).decode('utf-8'))
         conversation = [(item['role'], item['content']) for item in request['messages']]
-        reply = self.model.reply(conversation, stop_event=self.stop_event) # TODO: Temperature? max num tokens?
+
+        reply = self.model.reply(
+            conversation,
+            temperature=0,
+            max_new_tokens=128,
+            stop_event=self.stop_event,
+        )
+
         self.send_response(200)
         self.end_headers()
         self.wfile.write(json.dumps(reply).encode('utf-8'))
 
-def evaluate_model(model_type, model_name, model_args, evaluation_id):
+def install():
+    new_environment = os.environ.copy()
+    new_environment['OPENAI_API_BASE'] = 'https://api.openai.com/v1' # For lateral thinking puzzle
+
+    if os.path.exists('.tmp/AgentBench/installation-is-done'):
+        new_environment['PATH'] = os.path.join(os.getcwd(), '.tmp/AgentBench/venv/bin') + ':' + os.environ['PATH']
+        return new_environment
+
     os.makedirs('.tmp', exist_ok=True)
 
     # Clone repository, setup virtual environment
 
-    subprocess.run(['git', 'clone', 'https://github.com/FastEval/AgentBench.git'], cwd='.tmp')
+    subprocess.run(['git', 'clone', 'https://github.com/THUDM/AgentBench.git'], cwd='.tmp')
     subprocess.run(['python3', '-m', 'venv', 'venv'], cwd='.tmp/AgentBench')
 
-    new_environment = os.environ.copy()
     new_environment['PATH'] = os.path.join(os.getcwd(), '.tmp/AgentBench/venv/bin') + ':' + os.environ['PATH']
-    new_environment['OPENAI_API_BASE'] = 'https://api.openai.com/v1' # For lateral thinking puzzle
 
     subprocess.run(['pip', 'install', '--upgrade', 'pip'], env=new_environment)
     subprocess.run(['pip', 'install', '-r', 'requirements.txt'], cwd='.tmp/AgentBench', env=new_environment)
+    subprocess.run(['pip', 'install', 'multiprocess'], cwd='.tmp/AgentBench', env=new_environment)
 
     # Build docker containers for tasks that require them
 
@@ -70,12 +83,17 @@ def evaluate_model(model_type, model_name, model_args, evaluation_id):
     subprocess.run(['pip', 'install', 'datasets'], env=new_environment) # Missing dependency
     subprocess.run(['pip', 'install', 'torch'], env=new_environment) # Missing dependency
 
+    os.close(os.open('.tmp/AgentBench/installation-is-done', os.O_CREAT))
+
     # Task: Knowledge Graph
 
     # This setup is way too complicated.
     # I'm going to skip this task for now.
 
-    # Run the inference
+    return new_environment
+
+def evaluate_model(model_type, model_name, model_args, evaluation_id):
+    new_environment = install()
 
     model = create_model(model_type, model_name, model_args)
 
@@ -88,34 +106,68 @@ def evaluate_model(model_type, model_name, model_args, evaluation_id):
     agent_file = os.path.join(os.getcwd(), 'evaluation', 'benchmarks', 'agentbench_agent.yaml')
 
     tasks = {
-        # 'os_interaction': 'configs/tasks/os_interaction/dev.yaml', # Mostly runs, but sometimes stuck in the end. Also completely different scores from paper
-        # 'dbbench': 'configs/tasks/dbbench/dev.yaml', # MySQL connection error towards the end
-        # 'lateral-thinking-puzzle': 'configs/tasks/lateralthinkingpuzzle/dev.yaml', # ZeroDivisionError
-        # 'lateral-thinking-puzzle-zh': 'configs/tasks/lateralthinkingpuzzle_zh/dev.yaml', # todo
-        # 'knowledge-graph': 'configs/tasks/knowledgegraph/dev.yaml', # Excluded for now
-        # 'alfworld': 'configs/tasks/alfworld/dev.yaml', # Doesn't work
-        # 'mind2web': 'configs/tasks/mind2web/dev.yaml', # todo
-        # 'card-game': 'configs/tasks/card_game/dev.yaml', # Progress bar stuck at 0
+        'os_interaction': {
+            # Sometimes runs, but sometimes stuck in the end.
+            # Getting a HTTP timeout then.
+            # Also completely different scores from paper
+            'config_file': 'configs/tasks/os_interaction/dev.yaml',
+            'num_workers': 26,
+        },
+        'dbbench': { # MySQL connection error and lots of "not available" stuff before.
+            'config_file': 'configs/tasks/dbbench/dev.yaml',
+            'num_workers': 15,
+        },
+        'lateral-thinking-puzzle': { # ZeroDivisionError
+            'config_file': 'configs/tasks/lateralthinkingpuzzle/dev.yaml',
+            'num_workers': 50,
+        },
+        'lateral-thinking-puzzle-zh': { # todo
+            'config_file': 'configs/tasks/lateralthinkingpuzzle_zh/dev.yaml',
+            'num_workers': 50,
+        },
+        'knowledge-graph': { # Excluded for now
+            'config_file': 'configs/tasks/knowledgegraph/dev.yaml',
+            'num_workers': 50,
+        },
+        'alfworld': { # Doesn't work
+            'config_file': 'configs/tasks/alfworld/dev.yaml',
+            'num_workers': 50,
+        },
+        'mind2web': { # todo
+            'config_file': 'configs/tasks/mind2web/dev.yaml',
+            'num_workers': 50,
+        },
+        'card-game': { # Progress bar stuck at 0
+            'config_file': 'configs/tasks/card_game/dev.yaml',
+            'num_workers': 16,
+        },
     }
 
+    included_tasks = ['card-game']
+
     scores = {}
-    for task_name, task_config_file in tasks.items():
-        output_directory = os.path.abspath(os.path.join('.tmp', 'agentbench', str(uuid.uuid4())))
+    for task_name, task_information in tasks.items():
+        if task_name not in included_tasks:
+            continue
 
-        subprocess.run([
-            'python', 'eval.py',
-            '--task', task_config_file,
-            '--agent', agent_file,
-            '--workers', str(os.cpu_count()),
-            '--output', output_directory,
-        ], env=new_environment, cwd='.tmp/AgentBench')
+        scores[task_name] = []
+        for i in range(10):
+            output_directory = os.path.abspath(os.path.join('.tmp', 'agentbench', str(uuid.uuid4())))
 
-        results_file = os.path.join(output_directory, 'results.json')
-        with open(results_file) as f:
-            results = json.load(f)
+            subprocess.run([
+                'python3', 'eval.py',
+                '--task', task_information['config_file'],
+                '--agent', agent_file,
+                '--workers', str(task_information['num_workers']),
+                '--output', output_directory,
+            ], env=new_environment, cwd='.tmp/AgentBench')
 
-        score = results['score']['overall']['acc']
-        scores[task_name] = score
+            results_file = os.path.join(output_directory, 'results.json')
+            with open(results_file) as f:
+                results = json.load(f)
+
+            score = results['score']['overall']['acc']
+            scores[task_name].append(score)
 
     print(scores)
 
