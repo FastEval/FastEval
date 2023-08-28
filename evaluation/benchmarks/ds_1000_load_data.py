de0a3b9bdeebaa1eb52e94b130917c952dbe4219
Til Jasper Ullrich <git@tju.me>
2023-08-28 03:59:44 +0000

    WizardCoder: 0.3091

--- evaluation/benchmarks/ds_1000_load_data.py
+++ evaluation/benchmarks/ds_1000_load_data.py
@@ -8,8 +8,6 @@ import ds1000
 data = ds1000.DS1000Dataset('../ds1000_data/ds1000_data').data
 output = {}
 for k, v in data.items():
-    if k != 'Matplotlib':
-        continue # does not support insertion
     output[k] = []
     for item in v:
         output[k].append({ 'prompt': item['prompt'], 'reference': item['reference_code'] })
