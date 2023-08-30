83fe3a3d21a8855b9294b980ff1ebfd8f516911c
Til Jasper Ullrich <git@tju.me>
2023-08-30 02:26:04 +0200

    Add explanation & fix some links

--- website/benchmarks/ds1000.js
+++ website/benchmarks/ds1000.js
@@ -2,9 +2,26 @@ import { fetchEvaluations, fetchFiles, round } from '../utils.js'
 import { createTextE } from '../components/text.js'
 import { createModelLinkE } from '../components/model-link.js'
 import { createTableScoreCell } from '../components/table-score-cell.js'
+import { createBackToMainPageE } from '../components/back-to-main-page.js'
 
 export async function createE(baseUrl) {
     const containerE = document.createElement('div')
+
+    containerE.appendChild(createBackToMainPageE('← Back to code table', { benchmark: 'code' }))
+
+    const explanationE = document.createElement('div')
+    containerE.appendChild(explanationE)
+    explanationE.classList.add('ds1000__explanation')
+    const ds1000LinkE = document.createElement('a')
+    ds1000LinkE.textContent = 'DS-1000'
+    ds1000LinkE.href = 'https://ds1000-code-gen.github.io/'
+    explanationE.append(
+        ds1000LinkE,
+        createTextE(' is a benchmark for evaluating the abilities of a model to understand python data science code. '
+            + 'The benchmark mostly (except matplotlib) consists of tasks where the model has to insert a missing piece into existing incomplete code. '
+            + 'The original DS-1000 benchmark is only designed for models that have been trained with special tokens on insertion & completion. '
+            + 'Here, we have modified it so that it can work with any chat language model.'),
+    )
 
     const evaluations = await fetchEvaluations(baseUrl)
     const scores = await fetchFiles(baseUrl, evaluations, 'ds1000', 'scores.json')
